7497917631c72b483d68619580786372a051bb2b
Roland D. San Nicolas <rdsannicolas@gmail.com>
2015-10-14 14:57:00 -0700

    Chart is working!

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 //image constructor
-function EvilImage(src, character, movie, vote) {
+function EvilImage(src, label, movie, value, color, highlight) {
   this.src = src;
-  this.character = character;
+  this.label = label;
   this.movie = movie;
-  this.vote = 0;
+  this.value = value;
+  this.color = "#F7464A",
+  this.highlight = "#FF5A5E"
 }
 
-function createEvil(img, name, movie, vote) {
-  var x = new EvilImage(img, name, movie, vote);
+function createEvil(img, name, movie, value, color, highlight) {
+  var x = new EvilImage(img, name, movie, value, color, highlight);
   evilCharacters.push(x);
 }
 
@@ -42,6 +44,8 @@ createEvil("img/theshining-jacktorrance.jpg", "Jack Torrance", "The Shining", 0)
 createEvil("img/Cruella-de-vil-101-dalmatians.jpg", "Cruella de Vil", "101 Dalmatians", 0);
 createEvil("img/Psycho-NormanBates.jpg", "Norman Bates", "Psycho", 0);
 createEvil("img/wizardofoz-TheWickedWitchoftheWest.jpg", "The Wicked Witch of the West", "The Wizard of Oz", 0);
+console.log(evilCharacters[0].value);
+
 
 //Generate a random number
 var randomNumber = function() {
@@ -67,7 +71,7 @@ var randomImg = function(){
   var randomPhoto1 = (evilCharacters[random1]);
   document.getElementById("photoGen1").src = randomPhoto1["src"];
   var title1 = document.getElementById("photoName1");
-  title1.appendChild(document.createTextNode(randomPhoto1["character"]));
+  title1.appendChild(document.createTextNode(randomPhoto1["label"]));
   var movie1 = document.getElementById("movie1");
   movie1.appendChild(document.createTextNode(randomPhoto1["movie"]));
   // console.log(title1);
@@ -76,7 +80,7 @@ var randomImg = function(){
   var randomPhoto2 = (evilCharacters[random2]);
   document.getElementById("photoGen2").src = randomPhoto2["src"];
   var title2 = document.getElementById("photoName2");
-  title2.appendChild(document.createTextNode(randomPhoto2["character"]));
+  title2.appendChild(document.createTextNode(randomPhoto2["label"]));
   var movie2 = document.getElementById("movie2");
   movie2.appendChild(document.createTextNode(randomPhoto2["movie"]));
   // console.log(title2);
@@ -91,8 +95,9 @@ var nextPhoto1 = function(e) {
   //counter for votes
   // console.log("Left was clicked");
   // console.log("Counter check random number: " + random1);
-  evilCharacters[random1].vote+=1;
-  console.log(evilCharacters[random1].character + " vote TOTAL: " + evilCharacters[random1].vote);
+  evilCharacters[random1].value+=1;
+  console.log(evilCharacters[random1].label + " vote TOTAL: " + evilCharacters[random1].value);
+
 
   var titleRemove1 = document.getElementById("photoName1");
   titleRemove1.removeChild(titleRemove1.childNodes[0]);
@@ -107,15 +112,23 @@ var nextPhoto1 = function(e) {
   // console.log(movieRemove2.childNodes);
 //recall img function to insert new photos
   randomImg();
+  myPieChart = new Chart(context).Pie(evilCharacters);
 };
+
+// function findInArray(key) {
+//   for (var i in dataChart) {
+//     if (dataChart[i].label === key) return array[i];
+//   }
+// }
 
 var nextPhoto2 = function(e) {
   e.preventDefault();
   //counter for votes
   // console.log("Right was clicked");
   // console.log("Counter check random number: " + random2);
-  evilCharacters[random2].vote+=1;
-  console.log(evilCharacters[random2].character + " vote TOTAL: " + evilCharacters[random2].vote);
+  evilCharacters[random2].value+=1;
+  console.log(evilCharacters[random2].label + " vote TOTAL: " + evilCharacters[random2].value);
+
 
   var titleRemove2 = document.getElementById("photoName1");
   titleRemove2.removeChild(titleRemove2.childNodes[0]);
@@ -130,6 +143,7 @@ var nextPhoto2 = function(e) {
   // console.log(movieRemove2.childNodes);
 //recall img function to insert new photos
   randomImg();
+  myPieChart = new Chart(context).Pie(evilCharacters);
 };
 
 //voting system event listener
@@ -139,31 +153,73 @@ vote1.addEventListener('click', nextPhoto1, false);
 var vote2 = document.getElementById('photoGen2');
 vote2.addEventListener('click', nextPhoto2, false);
 
+
 //array of data for chart
-var data = {
+// var dataChart = [
+//     {
+//         value: evilCharacters[0].vote,
+//         color:"#F7464A",
+//         highlight: "#FF5A5E",
+//         label: evilCharacters[0].character
+//     },
+//     {
+//         value: evilCharacters[1].vote,
+//         color:"#F7464A",
+//         highlight: "#FF5A5E",
+//         label: evilCharacters[1].character
+//     },
+//     {
+//         value: evilCharacters[2].vote,
+//         color:"#F7464A",
+//         highlight: "#FF5A5E",
+//         label: evilCharacters[2].character
+//     }
+// ]
+var context = document.getElementById('voteChart').getContext('2d');
+var myPieChart = new Chart(context).Pie(evilCharacters);
+
+
+
+/*var data = {
     labels: [evilCharacters[0].character, evilCharacters[1].character, evilCharacters[2].character, evilCharacters[3].character, evilCharacters[4].character, evilCharacters[5].character, evilCharacters[6].character, evilCharacters[7].character, evilCharacters[8].character, evilCharacters[9].character, evilCharacters[10].character, evilCharacters[11].character, evilCharacters[12].character, evilCharacters[13].character, evilCharacters[14].character, evilCharacters[15].character, evilCharacters[16].character, evilCharacters[17].character, evilCharacters[18].character, evilCharacters[19].character, evilCharacters[20].character, evilCharacters[21].character, evilCharacters[22].character, evilCharacters[23].character, evilCharacters[24].character, evilCharacters[25].character],
     datasets: [
-        {
+    {
+      value: evilCharacters[0].vote,
+      Color: "rgba(220,220,220,0.5)",
+
+    }
+
+
+
+
+      {
+
             label: "Votes",
             fillColor: "rgba(220,220,220,0.5)",
             strokeColor: "rgba(220,220,220,0.8)",
             highlightFill: "rgba(220,220,220,0.75)",
             highlightStroke: "rgba(220,220,220,1)",
-            votes: [5, 2, 3, 1]
+            votes: [evilCharacters.vote]
         }
+
     ]
-};
+};*/
 
 //putting data chart into html
-var context = document.getElementById('voteChart').getContext('2d');
+
+
+
+/*var context = document.getElementById('voteChart').getContext('2d');
 var myBarChart = new Chart(context).Bar(data, {
   //animation options
   scaleBeginAtZero : true,
   scaleGridLineColor : "rgba(0,0,0,.05)",
   barValueSpacing : 5,
   barDatasetSpacing : 1
-});
+});*/
 
+/*
 myBarChart.datasets[0].votes[2].value = 50;
 // Would update the first dataset's value of 'March' to be 50
 myBarChart.update();
+*/
